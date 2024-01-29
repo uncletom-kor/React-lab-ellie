ecdb0475fc52df5ca3b84f22c4ae13d5cc7a68ef
github uncletom-kor <github uncletom.kor@gmail.com>
2024-01-29 15:06:20 +0900

    버튼1 캡션명 변경

--- src/components/Button1.jsx
+++ src/components/Button1.jsx
@@ -4,5 +4,5 @@ import React from 'react';
 import styles from './Button1.module.css';
 
 export default function Button1() {
-    return <button className={styles.button}>Button1</button>;
+    return <button className={styles.button}>새버튼</button>;
 }
